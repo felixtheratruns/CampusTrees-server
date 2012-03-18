f36b242149a8c0a77b70b4ac15e21e03cde41f48
Jesse Determann <jesse@treeproject.(none)>
2012-03-17 20:25:41 -0400

    Added class for trees being accessed by admin this should prbably just extend tree

---
+++ classes/admin_tree.inc.php
@@ -0,0 +1,68 @@
+<?php
+//Requirements
+require_once(GCTOOLS_DIR . "database.inc.php");
+
+class AdminTree { 
+	protected $recid;
+	protected $id;		//Tree ID
+	protected $sid;		//Species ID
+	protected $lat;		//GPS Latitude
+	protected $long;	//GPS Longitude
+	protected $aid;		//Area id
+	protected $qid;		//Quadrant ID
+	protected $dbh;
+	protected $dcrn;
+	protected $dtree;
+	protected $height;
+	protected $cid;		//Crown ID
+	protected $cw1;		//Crown width 1
+	protected $cw2;		//Crown width 2
+	protected $ca;		//Crown area
+	protected $rem;		//Tree removed?
+	protected $com;		//Comments
+        protected $createddate;
+        protected $creatorid;
+
+
+        //Contructor
+        public function AdminTree($id) {
+            $this->setId($id);
+
+        }
+        public function getProperties() {
+            return get_object_vars($this);
+        }
+        public function ToJSON() {
+            return json_encode(get_object_vars($this));
+        }
+        public function setId($id) {
+            $dbres = new MySQL(SQL_HOST, SQL_USER, SQL_PASS, SQL_DB);
+            $res = mysql_fetch_assoc($dbres->query("SELECT TRecId, TTreeId,
+                TSpeciesId, TLat, TLong, TAreaId, TQuadId, TDBH, TDistCrn, TDistTree,
+                THeight,TCrwnId, TCrwnWidth1, TCrwnWidth2, TCrwnArea, TRemoved,
+                TComments, TRecCreatedDate, TRecCreatorId
+                FROM Tree where TTreeId = " . $dbres->escapeString($id) . ""));
+            //Now set all of the other attributes
+            $this->recid = (int)$res['TRecId'];
+            $this->id = (int)$res['TTreeId'];
+            $this->sid = (int)$res['TSpeciesId'];
+            $this->lat = (double)$res['TLat'];
+            $this->long = (double)$res['TLong'];
+            $this->aid = (int)$res['TAreaId'];
+            $this->qid = (int)$res['TQuadId'];
+            $this->dbh = (double)$res['TDBH'];
+            $this->dcrn = (double)$res['TDistCrn'];
+            $this->dtree = (double)$res['TDistTree'];
+            $this->height = (double)$res['THeight'];
+            $this->cid = (int)$res['TCrwnId'];
+            $this->cw1 = (double)$res['TCrwnWidth1'];
+            $this->cw2 = (double)$res['TCrwnWidth2'];
+            $this->ca = (double)$res['TCrwnArea'];
+            $this->rem = (bool)$res['TRemoved'];
+            $this->com = $res['TComments'];
+            $this->createddate = $res['TRecCreatedDate'];
+            $this->creatorid = (int)$res['TRecCreatorId'];
+            return True;
+        }     
+}
+?>
