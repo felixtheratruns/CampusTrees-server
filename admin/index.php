de9a9ba1f6c75eaa3a324139001476c346fbb4a7
Jesse Determann <jesse@treeproject.(none)>
2012-03-28 12:20:25 -0400

    Renamed root index.php to test.php meow

--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,18 @@
 This is an admin page!
+<?php
+//Check if a user is logged into the admin section
+$loggedIn = FALSE;
+
+//User is not logged in
+if ($loggedIn !== TRUE) {
+?>
+This is the admin page!
+<?php
+}
+//User is logged in
+else {
+?>
+You are currently logged in.
+<?php
+}
+?>
