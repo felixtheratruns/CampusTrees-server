00c3ae4a8f493a6d77189f5900323db4d7509e41
Jesse Determann <jesse@treeproject.(none)>
2012-04-02 23:45:27 -0400

    Forgot to add a classfile

---
+++ classes/EntityUpdateTable.inc.php
@@ -0,0 +1,61 @@
+<?php
+//Requirements
+require_once(GCTOOLS_DIR . "database.inc.php");
+
+class EntityUpdateRecord {
+        protected $uname;
+        protected $uid;
+        protected $rem;
+        protected $date;
+
+        public function EntityUpdateRecord($username, $userid, $removed, $updatedate) {
+           $this->uname = $username;
+           $this->uid = $userid;
+           $this->rem = $removed;
+           $this->date = $updatedate;
+        }
+
+        public function getProperties() {
+            $properties = get_object_vars($this);
+            return $properties;
+        }
+}
+class EntityUpdateTable { 
+//General Properties
+        private $dbres;
+        //Contructor
+        public function EntityUpdateTable() {
+            $this->dbres = new MySQL(SQL_HOST, SQL_USER, SQL_PASS, SQL_DB);
+        }
+
+        public function logUpdate($eid, $refid, $upid, $remov) {
+            $query = "INSERT INTO EntityUpdate (EUEntityTypeId, EURefRecId,
+                        EURefRecUpdatorId, EURefRecRemoved)
+                        VALUES ({$this->dbres->escapeString($eid)},
+                        {$this->dbres->escapeString($refid)},
+                        {$this->dbres->escapeString($upid)},
+                        {$remov})";
+//          echo $query;
+            $this->dbres->query($query);
+        }
+
+        public function getEditHistory($eid, $refid) {
+            $query = "SELECT UUserName, UUserId,
+                      EURefRecRemoved, EURefRecUpdatedDate
+                      FROM EntityUpdate INNER JOIN User ON (EURefRecUpdatorId = UUserId)
+                      WHERE EUEntityTypeId = {$this->dbres->escapeString($eid)} 
+                      AND EURefRecId = {$this->dbres->escapeString($refid)}";
+//          echo $query;
+            $res = $this->dbres->query($query);
+            $i = 0;
+            $hist = array();
+            while($row = mysql_fetch_assoc($res)) {
+                $euRec = new EntityUpdateRecord($row['UUserName'], $row['UUserId'],
+                  $row['EURefRecRemoved'], $row['EURefRecUpdatedDate']);
+                $hist[$i] = $euRec->getProperties();
+                $i++;
+            }
+            return $hist;
+        }
+}
+?>
