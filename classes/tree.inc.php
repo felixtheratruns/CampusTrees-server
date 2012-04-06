2330d760d1120f917657b9b5ee93443d4a8da30f
Jesse Determann <jesse@treeproject.(none)>
2012-04-05 22:10:10 -0400

    more docblock bs

--- classes/tree.inc.php
+++ classes/tree.inc.php
@@ -17,13 +17,20 @@ class Tree {
 
         //Generated properties
         protected $vol;         //Tree volumne in BoardFeet
-        protected $greenwt;     /** Calculated Green Weight of Tree */
-        protected $drywt;     /** Calculated Dry Weight of Tree */
-        protected $carbonwt;     /** Calculated Weight of Carbon in Tree */
-        protected $co2seqwt;     /** Calculated Weight of CO2 Sequestered in Tree */
-        protected $age;     /** Calculated Age of Tree */
-        protected $co2pyear;     /** Calculated CO2 Sequestered Per Year */
-        protected $crownarea;     /** Calculated Area of Crown */
+/** Calculated Green Weight of Tree */
+        protected $greenwt;     
+/** Calculated Dry Weight of Tree */
+        protected $drywt;     
+/** Calculated Weight of Carbon in Tree */
+        protected $carbonwt;     
+/** Calculated Weight of CO2 Sequestered in Tree */
+        protected $co2seqwt;     
+/** Calculated Age of Tree */
+        protected $age;     
+/** Calculated CO2 Sequestered Per Year */
+        protected $co2pyear;     
+/** Calculated Area of Crown */
+        protected $crownarea;     
 
 //Admin properties
         protected $recid;        //TRecId
