7bd6afcb2886dc67d8f6c0db4a6f2fe486231761
giga1699 <giga1699@gmail.com>
2012-04-17 21:46:46 -0400

    Renamed index.html to index.php, and created the pages directory to store included pages

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by Free CSS Templates
+http://www.freecsstemplates.org
+Released for free under a Creative Commons Attribution 2.5 License
+
+Name       : Mobility   
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20120203
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>UofL Trees</title>
+<link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="wrapper">
+	<div id="header-wrapper">
+		<div id="header">
+			<div id="logo">
+				<h1><a href="#">UofL Trees</a></h1>
+				<p>A UWRL Project</p>
+			</div>
+		</div>
+	</div>
+	<!-- end #header -->
+	<div id="menu">
+		<ul>
+			<li class="current_page_item"><a href="#">Homepage</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="mapView.html">Tree Map</a></li>
+		</ul>
+	</div>
+	<!-- end #menu -->
+	<div id="page">
+		<div id="page-bgtop">
+			<div id="page-bgbtm">
+				<div id="content">
+					<?php include("pages/main.php"); ?>
+				</div>
+				<!-- end #content -->
+				<div id="sidebar">
+					<ul>
+						<li>
+							<h2>Tree Species</h2>
+							<ul>
+								<li><a href="#">Aliquam libero</a></li>
+								<li><a href="#">Consectetuer adipiscing elit</a></li>
+								<li><a href="#">Metus aliquam pellentesque</a></li>
+								<li><a href="#">Suspendisse iaculis mauris</a></li>
+								<li><a href="#">Urnanet non molestie semper</a></li>
+								<li><a href="#">Proin gravida orci porttitor</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+				<!-- end #sidebar -->
+				<div style="clear: both;">&nbsp;</div>
+			</div>
+		</div>
+	</div>
+	<!-- end #page -->
+</div>
+<div id="footer">
+	<p>Copyright (c) 2012 UofL Urban Wildlife Research Lab. All rights reserved. Design by <a href="http://www.freecsstemplates.org/"> CSS Templates</a>.</p>
+</div>
+<!-- end #footer -->
+</body>
+</html>
---
+++ pages/main.php
@@ -0,0 +1,8 @@
+					<div class="post">
+                                                <h2 class="title"><a href="#">Welcome</a></h2>
+                                                <p class="meta">Posted by <a href="#">UWRL</a> on March 28, 2012</p>
+                                                <div class="entry">
+							<p>Welcome to the UWRL Tree website! We have a lot of trees here at the University of Louisville, and want to share some great information about them with you. Please check out our <a href="mapView.html">Tree Map</a> to see where all the trees on campus are!</p>
+                                                </div>
+                                        </div>
+                                        <div style="clear: both;">&nbsp;</div>
