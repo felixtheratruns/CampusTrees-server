4b6e1c0b9707ca567eae9de850b1b3198ed70454
RADRachel <RAD.April.Rachel@gmail.com>
2012-06-05 18:41:56 -0400

    Wildlife Facts list page availible

---
+++ admin/modules/list_wfact.php
@@ -0,0 +1,34 @@
+<?php
+require_once('../../config.inc.php');
+require_once(ROOT_DIR . 'classes/WildlifeFactsTable.inc.php');
+require_once(ROOT_DIR . 'admin/modules/auth.inc.php');
+
+if (isset($debug)) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+}
+$wTable = new WildlifeFactsTable();
+$html = "<html>
+<a href=\"add_wfact.php\">Add a WildlifeFacts Item</a></br>";
+
+
+    $i = 0;
+    $wList = $wTable->GetFacts();
+    $html .= "<table border=\"1\">";
+    $html .= "<tr><th>Title</th><th>Body</th><th>Date</th><th>User</th><th>Remove</th></tr>";
+    foreach ($wList as $row) {
+        $html .= "<tr>";
+        $html .= "<td>{$row['title']}</td>";
+        $html .= "<td>{$row['body']}</td>";
+        $html .= "<td>{$row['date']}</td>";
+        $html .= "<td><a href=\"" . HOME;
+        $html .= "admin/modules/edit_user.php?uid={$row['uid']}\">{$row['uname']}</a></td>";
+        $html .= "<td><a href=\"" . HOME;
+        $html .= "admin/modules/edit_wfact.php?wid={$row['wid']}\">Delete</a></td>";
+        $html .= "</tr>";
+    }
+    $html .= "</table>";
+
+$html .= "</html>";
+echo $html;
+?>
