0bb516742b22103a2ebe868fc69d728bb18cb9f3
RADRachel <RAD.April.Rachel@gmail.com>
2012-08-31 18:47:37 -0400

    Forgot these new files

---
+++ admin/modules/edit_zone.php
@@ -0,0 +1,297 @@
+<?php
+require_once('../../config.inc.php');
+require_once(ROOT_DIR . 'classes/ZonePointTable.inc.php');
+require_once(ROOT_DIR . 'admin/modules/auth.inc.php');
+$debug=1;
+if (isset($debug)) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+}
+
+$zpTable = new ZonePointTable();
+
+$add = 0;
+if (isset($_GET['add'])) {
+    $add += $_GET['add'];
+}
+
+if (isset($_GET['zpid'])) {
+    $zpid = $_GET['zpid'];
+}
+else {$zpid = 0;}
+/*
+if (isset($_GET['new'])) {
+    #Some shit to figure the new zpid
+    #Already being done by calling page (list_zones) but may be a better way
+}
+*/
+if (isset($_POST['zpid'])) {
+    $zpid = $_POST['zpid'];
+    if ($zp = $zpTable->GetZone($zpid)) {
+        $i=0;
+    
+        $points = $zp['points'];
+        $updateList = array();
+        foreach ($points as $row) {
+            $pid = $row['pid'];
+            $lat = $_POST["lat{$row['pid']}"];
+            $long = $_POST["long{$row['pid']}"];
+            if (strcmp($lat, $row['lat']) != 0) {
+                echo "{$row['pid']} lat updated!<br>
+                        {$row['lat']} to {$lat}<br>";
+                 $updateList[$i] = array('pid' => (int)$row['pid'],
+                                    'type' => (string)"ZPLat",
+                                    'coord' => (string)$lat);
+                 $i++;
+            }
+            if (strcmp($long, $row['long']) != 0) {
+                echo "{$row['pid']} long updated!<br>";
+                 $updateList[$i] = array('pid' => (int)$row['pid'],
+                                    'type' => (string)"ZPLong",
+                                    'coord' => (string)$long); # Need to pass updateList to the thing
+                 $i++;
+            }
+        }
+        if($zpTable->UpdateZone($zpid, $updateList)) echo "<h3>Updated!</h3>";
+    }
+    if(isset($_POST['npid'])) {
+        $npid = $_POST['npid'];
+        $nlat = $_POST['nlat'];
+        $nlong = $_POST['nlong'];
+        if($zpTable->NewZonePoint($zpid, $npid, $nlat, $nlong)) echo "<h3>Point Added!</h3>";
+        else echo "<h3>Error Updating!</h3>";
+    }
+
+}
+
+$zp = array('points' => array());
+if (isset($_GET['new']) || $zp = $zpTable->GetZone($zpid) )  {
+
+    $html = "<html>
+    <form name =\"edit_zone\"
+      action=\"edit_zone.php\" method=\"POST\">
+        <input type=\"hidden\" name=\"zpid\" value=\"{$zpid}\">"; 
+    
+    $pid = 0;
+    $points = $zp['points'];
+    $html .= "<table border=\"1\">";
+    $html .= "<tr><th>Point ID</th><th>Lat</th><th>Long</th></tr>";
+    if (count($points) > 0) {
+        foreach ($points as $row) {
+            $pid = $row['pid'];
+            $html .= "<tr>";
+            $html .= "<td>{$pid}</td>";
+            $html .= "<td><input type=\"text\" name=\"lat{$pid}\"
+                        value=\"{$row['lat']}\"</td>";
+            $html .= "<td><input type=\"text\" name=\"long{$pid}\"
+                        value=\"{$row['long']}\"</td>";
+            $html .= "</tr>";
+        }
+    }
+    if (isset($_GET['new']) || $add > 0) {
+        $pid++;
+        $html .= "<tr>";
+        $html .= "<td>{$pid}</td>";
+        $html .= "<td><input type=\"text\" name=\"nlat\"
+                    value=\"0\"</td>";
+        $html .= "<td><input type=\"text\" name=\"nlong\"
+                    value=\"0\"</td>";
+        $html .= "</tr>
+                    <input type=\"hidden\" name=\"npid\" value=\"{$pid}\">"; 
+    }
+    $html .= "</table>";
+    
+        
+    if (!isset($_GET['new']) && !isset($_GET['add'])) $html .= "<a href=\"edit_zone.php?zpid={$zpid}&add=1\">Add a Point</a></br>";
+    $html .= "<input type=\"submit\" value=\"Submit\"><br>
+                <a href=\"list_zones.php\">Back to Zone List</a></html>";
+    echo $html;
+}
+else echo "<html><h3>Invalid Zone Id</h3></html>"
+?>
+<?php
+/*
+require_once('../../config.inc.php');
+require_once(ROOT_DIR . 'classes/TreeTable.inc.php');
+require_once(ROOT_DIR . 'classes/tree.inc.php');
+require_once(ROOT_DIR . 'classes/SpeciesTable.inc.php');
+require_once(ROOT_DIR . 'classes/EntityUpdateTable.inc.php');
+require_once(ROOT_DIR . 'admin/modules/auth.inc.php');
+
+if (isset($debug)) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+}
+if (isset($_POST['id'])) { //IF Handling an update:
+    $id = $_POST['id'];
+    $t = new tree($id);
+    $tree = $t->getProperties(True);
+    
+    $lat = $_POST['lat'];
+    $long = $_POST['long'];
+    $sid = $_POST['sid'];
+    $height = $_POST['height'];
+    $removed = false;
+    $comments = $_POST['comments'];
+    $dbh = $_POST['dbh'];
+    $cw1 = $_POST['cw1'];
+    $cw2 = $_POST['cw2'];
+    $area = $_POST['area'];
+    $quad = $_POST['quad'];
+    $dcrn = $_POST['dcrn'];
+    $dtree = $_POST['dtree'];
+    $crwnid = $_POST['crwnid'];
+    
+    if (isset($_POST['removed'])) {
+        $removed = true;
+    }
+    
+    $fields['id'] = $tree['id'];
+    $fields['uid'] = $uid;
+    
+    if ($tree['lat'] != $lat) {
+        $fields['lat'] = $lat;
+    }
+    
+    if ($tree['long'] != $long) {
+        $fields['long'] = $long;
+    }
+    
+    if ($tree['sid'] != $sid) {
+        $fields['sid'] = $sid;
+    }
+    
+    if ($tree['height'] != $height) {
+        $fields['height'] = $height;
+    }
+    
+    if ($tree['removed'] != $removed) {
+        if ($removed) { $fields['removed'] = 1;}
+        else { $fields['removed'] = 0; }
+    }
+    
+    if ($tree['comments'] != $comments) {
+        $fields['comments'] = $comments;
+    }
+    
+    if ($tree['dbh'] != $dbh) {
+        $fields['dbh'] = $dbh;
+    }
+    
+    if ($tree['cw1'] != $cw1) {
+        $fields['cw1'] = $cw1;
+    }
+    
+    if ($tree['cw2'] != $cw2) {
+        $fields['cw2'] = $cw2;
+    }
+    
+    if ($tree['area'] != $area) {
+        $fields['area'] = $area;
+    }
+    
+    if ($tree['quad'] != $quad) {
+        $fields['quad'] = $quad;
+    }
+    
+    if ($tree['dcrn'] != $dcrn) {
+        $fields['dcrn'] = $dcrn;
+    }
+    
+    if ($tree['dtree'] != $dtree) {
+        $fields['dtree'] = $dtree;
+    }
+    
+    if ($tree['crwnid'] != $crwnid) {
+        $fields['crwnid'] = $crwnid;
+    }
+    
+    $t->update($fields);
+    
+}
+if (!isset($_GET['t'])) {//No tree selected, so list trees
+    echo "<HEAD><meta http-equiv=\"REFRESH\" content=\"0;url=" . HOME ."admin/modules/list_tree.php\"></HEAD>";
+}
+else {//Tree selected - Display Form:
+    $id = $_GET['t'];
+    $t = new tree($id);
+    $sTable = new SpeciesTable();
+    $tree = $t->getProperties(True);
+    $species = $sTable->GetSpecies();
+    $euTable = new EntityUpdateTable();
+    ?>
+
+    <h1>Edit Tree</h1>
+    <a href="maintenance.php?t=<?php echo $tree['id'];?>">Maintenance</a><br />
+    <form name="edit_tree" action="edit_tree.php?t=<?php echo $tree['id']; ?>" method="POST">
+    <h2>Basic Information</h2>
+    Tree ID:<?php echo " {$tree['id']}"; ?><br />
+    <input type="hidden" name="id" value="<?php echo $tree['id'];?>">
+
+    <a href="list_tree.php">Tree List</a><br><br>
+    Tree Species: <select name="sid">
+    <?php 
+    foreach ($species as $s) {
+        $string = "<option value=\"{$s['sid']}\"";
+        if ($s['sid'] == $tree['sid']) {$string .= " selected";}
+        $string .= ">{$s['commonname']}</option>";
+        echo $string;
+    }
+    echo "</select> <a href=\"edit_species.php?sid={$tree['sid']}\">Edit Species</a>";
+    ?>
+    <br />
+
+    <h2>Calculated Data:</h2>
+    Age:<?php
+        echo " {$tree['age']}";
+        if ($tree['avged']) {echo "*";}
+    ?> Years<br />
+    Volume:<?php echo " {$tree['vol']}"; ?> Board Feet<br />
+    Green Weight:<?php echo " {$tree['greenwt']}"; ?> Pounds<br />
+    Dry Weight:<?php echo " {$tree['drywt']}"; ?> Pounds<br />
+    Weight of Carbon:<?php echo " {$tree['carbonwt']}"; ?> Pounds<br />
+    Weight of CO2(Lifetime):<?php echo " {$tree['co2seqwt']}"; ?> Pounds<br />
+    Weight of CO2 (per Year):<?php
+        echo " {$tree['co2pyear']}";
+        if ($tree['avged']) {echo "*";}
+    ?> Pounds<br />
+    Area of Crown:<?php echo " {$tree['crownarea']}"; ?> Square Feet<br />
+    <br>
+    
+    <h2>Location</h2>
+ <input type="text" size="10" name="lat" value="<?php echo $tree['lat'];?>"> Lat, <input type="text" size="10" name="long" value="<?php echo $tree['long'];?>"> Long<br />
+    Area: <input type="text" name="area" value="<?php echo $tree['area'];?>"><br />
+    Quad: <input type="text" name="quad" value="<?php echo $tree['quad'];?>"><br />
+    <br />
+
+    <h2>Measurements</h2>
+    Tree Height: <input type="text" name="height" value="<?php echo $tree['height'];?>"><br />
+    Tree DBH: <input type="text" name="dbh" value="<?php echo $tree['dbh'];?>"><br />
+    Tree Crown Width 1: <input type="text" name="cw1" value="<?php echo $tree['cw1'];?>"><br />
+    Tree Crown Width 2: <input type="text" name="cw2" value="<?php echo $tree['cw2'];?>"><br />
+    Dist Crown: <input type="text" name="dcrn" value="<?php echo $tree['dcrn'];?>"><br />
+    Dist Tree: <input type="text" name="dtree" value="<?php echo $tree['dtree'];?>"><br />
+    Crown Id: <input type="text" name="crwnid" value="<?php echo $tree['crwnid'];?>"><br />
+    <br />
+    
+    <h2>Additional Information</h2>
+    Tree Removed: <input type="checkbox" name="removed" value="true" <?php if ($tree['removed']) {echo " checked=\"checked\"";}?>><br />
+    <br />
+    
+    <h2>Comments</h2>
+    <textarea name="comments" rows="15" cols="120" ><?php echo $tree['comments'];?></textarea>
+    <br>
+    <input type="submit" value="Submit">
+    </form>
+    <br>
+    <?php
+    $hist = $euTable->getEditHistory(1, $tree['recid']);
+    foreach ($hist as $row) {
+        echo "Edited: {$row['date']} by 
+              <a href=\"edit_user.php?uid={$row['uid']}\">{$row['uname']}</a>";
+        if ($row['rem'] == 1) {echo " Tree Removed"; }
+        echo "<br>";
+    }
+}
+*/
+?>
---
+++ admin/modules/list_zones.php
@@ -0,0 +1,37 @@
+<?php
+require_once('../../config.inc.php');
+require_once(ROOT_DIR . 'classes/ZonePointTable.inc.php');
+require_once(ROOT_DIR . 'admin/modules/auth.inc.php');
+$debug=1;
+if (isset($debug)) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+}
+$zpTable = new ZonePointTable();
+$html = "<html>";
+
+
+    $i = 0;
+    $zpid = 0;
+    $zpList = $zpTable->GetZones();
+    $html .= "<table border=\"1\">";
+    $html .= "<tr><th>ZoneId</th><th>Number of Points</th><th>Edit</th></tr>";
+    foreach ($zpList as $row) {
+        $zpid = $row['zpid'];
+        $count = 0;
+        foreach ($row['points'] as $p) {
+            $count++;
+        }
+        $html .= "<tr>";
+        $html .= "<td>{$zpid}</td>";
+        $html .= "<td>{$count}</td>";
+        $html .= "<td><a href=\"" . HOME;
+        $html .= "admin/modules/edit_zone.php?zpid={$zpid}\">edit</a></td>";
+        $html .= "</tr>";
+    }
+    $zpid++;
+    $html .= "</table><br><a href=\"edit_zone.php?zpid={$zpid}&new=1\">Add a Zone</a>";
+
+$html .= "</html>";
+echo $html;
+?>
