0e783b5a758e69dbfb472471344a1ff3d600f047
joel <joel2.718@gmail.com>
2014-05-15 02:26:38 -0400

    added license files and comments

--- pages/main.php
+++ pages/main.php
@@ -1,3 +1,4 @@
+/** This file is part of server-side of the CampusTrees Project. It is subject to the license terms in the LICENSE file found in the top-level directory of this distribution. No part of CampusTrees Project, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the LICENSE file.*/
 					<div class="post">
                                                 <h2 class="title"><a href="#">Welcome</a></h2>
                                                 <p class="meta">Posted by <a href="#">UWRL</a> on March 28, 2012</p>
